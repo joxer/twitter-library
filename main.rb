4d1bac84306e0442205c7b6f5d53435d13cd9a60
joxer <joxer@nemesis.(none)>
2009-01-23 21:54:22 +0100

    uhlala

---
+++ main.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'uri'
+require 'net/http'
+
+module Tweet
+  
+  class Connect
+    
+    def initialize(name, passwd)
+
+      @name = name
+      @passwd = passwd
+
+    end
+
+    def status(data = "", format = "json")
+      if data.length > 140
+        
+        puts "message too long"
+      else
+        
+        url = URI.parse("http://twitter.com/statuses/update.#{format}")
+        req = Net::HTTP::Post.new(url.path)
+        req.basic_auth '#{@name}', '#{@passwd}'
+        req.set_form_data({"status" => "#{data}"}, ';')
+        res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
+        p res
+      end
+
+      
+      
+    end
+  end
+
+end
